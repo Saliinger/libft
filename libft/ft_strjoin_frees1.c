aa96faa180c6c9fd3671c01f6617fa61f2a67f31
Saliinger <alexisnoukan@gmail.com>
2024-10-23 14:06:05 +0200

    done lexer + redir parse

--- libft/ft_strjoin_frees1.c
+++ libft/ft_strjoin_frees1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin_FreeS1.c                                :+:      :+:    :+:   */
+/*   ft_strjoin_frees1.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 14:09:47 by anoukan           #+#    #+#             */
-/*   Updated: 2024/07/11 14:09:49 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/10/22 10:35:45 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,18 @@ char	*ft_strjoin_frees1(char *s1, char *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	i = ft_strlen(s1);
+	if (s1)
+		i = ft_strlen(s1);
+	else
+		i = 0;
 	j = ft_strlen(s2);
 	dest = (char *)malloc(i + j + 1);
 	if (dest == NULL)
 		return (free(s1), s1 = NULL, free(s2), s2 = NULL, NULL);
-	ft_strcpy(dest, (char *)s1);
+	if (s1)
+		ft_strcpy(dest, (char *)s1);
 	ft_strcpy(dest + i, (char *)s2);
-	free(s1);
+	if (s1)
+		free(s1);
 	return (dest);
 }
