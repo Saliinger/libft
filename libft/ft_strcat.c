092b84113b3b1bc5d476da2a9d6a61a25f4f15f2
Saliinger <alexisnoukan@gmail.com>
2024-12-11 12:00:18 +0100

    add ft_strcpy + ft_strcat

---
+++ libft/ft_strcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 11:58:59 by anoukan           #+#    #+#             */
+/*   Updated: 2024/12/11 11:59:17 by anoukan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strcat(char *dst, const char *src)
+{
+	size_t	dst_len;
+	size_t	i;
+
+	if (!dst && !src)
+		return (NULL);
+	if (!dst)
+		return ((char *)src);
+	if (!src)
+		return (dst);
+	dst_len = ft_strlen(dst);
+	i = 0;
+	while (src[i])
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst);
+}
