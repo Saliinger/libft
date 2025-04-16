eb146a6d0ec9d7b8b82b1134ba31084d29934975
Saliinger <alexisnoukan@gmail.com>
2025-04-17 01:44:41 +0200

    norm

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -1,28 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 01:44:19 by anoukan           #+#    #+#             */
+/*   Updated: 2025/04/17 01:44:20 by anoukan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char *ft_strndup(const char *s, size_t n)
+char	*ft_strndup(const char *s, size_t n)
 {
-    char *dup;
-    size_t i;
+	char	*dup;
+	size_t	i;
 
-    if (!s)
-        return (NULL);
-    
-    // Allouer de la mémoire pour la nouvelle chaîne (+1 pour le caractère de fin de chaîne '\0')
-    dup = (char *)malloc(sizeof(char) * (n + 1));
-    if (!dup)
-        return (NULL);
-
-    // Copier jusqu'à n caractères ou jusqu'à la fin de la chaîne d'origine
-    i = 0;
-    while (i < n && s[i])
-    {
-        dup[i] = s[i];
-        i++;
-    }
-
-    // Ajouter le caractère null à la fin de la chaîne dupliquée
-    dup[i] = '\0';
-
-    return (dup);
+	if (!s)
+		return (NULL);
+	dup = (char *)malloc(sizeof(char) * (n + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < n && s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
 }
